dabeeb1aa3bf4d3176cef7d1c7aed5d1df203500
brucejs777 <brucejs777@yahoo.com>
2019-03-04 23:40:47 -0500

    Merge branch 'master' of https://github.com/brucejs777/erase_i2c
    # Conflicts:
    #	.idea/deployment.xml

--- erase_i2c.js
+++ erase_i2c.js
@@ -1,28 +1,36 @@
 // erase_i2c.js
 
-// cloud9 proj folder from prev code (not this project) = debian@beaglebone:/var/lib/cloud9/_MY_PROJ
+
+// erase a 24C16 I2C memory
+
+// this was a cloud9 proj folder from prev code (not this project) = debian@beaglebone:/var/lib/cloud9/_MY_PROJ
 
 /*
+    webstorm notes
+
 ran in 13.1 secs for 2048 individual writes (no paging)
 
+    hardware notes
 NOTE: I HAD (before end of Feg '19) a 24C02 (256x8) strapped
 as address 0x51 (A2-A0 = pin3-1 = 001)
+( before we used BBB I2C port 2 on P9 pins 19=SCL and 20=SDA)
+but now using SCL=pin24 and SDA=pin26
 
 2-22-19 trying i2c bus 1 on pins 24 and 26 - use config-pin -a p9_24 i2c etc
 (must take original i2c mode off of pins 17-18 if set (but defaults to 'default')
 
-erase a 24C16 I2C memory
 note that the three address lines of the 24C16 A0-A2 are not used
 instead, the chip must be addressed from $A0 to $AE (8-bit) or $50 to $57
-note that the page size of the Microchip 24LC16B is 16 bytes
-the 24LC16B page write time is max 5ms
-( before we used BBB I2C port 2 on P9 pins 19=SCL and 20=SDA)
-but now using SCL=pin24 and SDA=pin26
+
 pullups not included on BBB; 3.3 Volt bus
 use 4.7k pullups to 3.3V and level translator or Schottkey diodes to +3.3V
 note: if connecting to  5V system then really only need pullups to 3.3V
 
-linux cmd line i2c tools: list busses; list devices; set; dump
+    i2c notes
+note that the page size of the Microchip 24LC16B is 16 bytes
+the 24LC16B page write time is max 5ms
+
+    linux i2c-tools cmd line tools: list busses; list devices; set; dump
 note bus number is zero-based; bbb uses bus 2 for p9-19/20 clk/data
 i2cdetect -l // list buses
 i2cdetect -r 2 // list devices found on bus
@@ -34,7 +42,7 @@ i2cdump -y 2 0x50 b // get data b=bytes, w=words
 const assert = require('assert');
 
 process.stdout.write(`process ver = ${process.version}`);
-process.stdout.write('\n\terase_i2c.js - erase a 24C16 memory on Beaglebone Black I2C bus 1');
+process.stdout.write('\n\terase_i2c.js - erase a 24C16 memory on Beaglebone Black I2C bus 1 \n');
 
 const i2c = require('i2c-bus');
 
@@ -47,12 +55,23 @@ const configPins = () => {
     let execResult;
     try {
         execResult = execSync('config-pin -a P9_24 i2c');
-        console.log(execResult);
+        console.log(execResult.toString());
         execResult = execSync('config-pin -a P9_26 i2c');
-        console.log(execResult);
+        console.log(execResult.toString());
     } catch (err) {
         console.log(`ERROR: problem configuring i2c pins; err code = ${er}\n`);
     }
+};
+
+
+// make array of various test patterns for memory testing
+// array can be put into a Buffer with Buffer.from(array)
+// x is fast and horiz, y is slower and vert, z is slowest
+const makeTestData = function(x, y, z) {
+    let result = [];
+    for (let i = 0; i < x; i++)
+        result[i] = i;
+    return result;
 };
 
 
@@ -60,16 +79,20 @@ const configPins = () => {
 // chip page size pageSize
 const eraseMemory = (
     busNum, i2cAddress,
-    numBytes, fillByte=0xff, pageSize=16
+    numBytes, fillByte, pageSize
 ) => {
     let startLoc = 0; // TODO: make this a param
     let bytesWritten = 0;
-    console.log(`\n\tErasing I2C memory device at address ${i2cAddress.toString(16)}`);
+    console.log(`\n\teraseMemory(): Erasing ${numBytes} of I2C memory device \n`
+    + `at address ${i2cAddress.toString(16)} on bus # ${busNum};  \n`
+    + `fill byte = ${fillByte}, page size = ${pageSize}`);
 
     const i2c1 = i2c.openSync(busNum);
-    const erasedData = Buffer.alloc(pageSize, fillByte);
+    // TODO: const erasedData = Buffer.alloc(pageSize, fillByte);
+    let erasedData = Buffer.from( makeTestData(pageSize, 0, 0) );
 
     for (let byteAddr = startLoc; byteAddr < startLoc + numBytes; byteAddr += pageSize) {
+        console.log(`try to write ${pageSize} bytes at byte addr ${byteAddr}`);
         bytesWritten = i2c1.i2cWriteSync(i2cAddress, pageSize, erasedData);
         waitFor(5);
         process.stdout.write(`number wrote = ${bytesWritten}\n`);
@@ -82,15 +105,15 @@ const eraseMemory = (
     i2c1.closeSync();
 };
 
-
+/*
 const readMemory = (
     busNum, i2cAddress,
     numBytes, fillByte=0xff, pageSize=16
 ) => {
-    assert.fail('not done yet');
+    assert.fail('readMemory() is not done yet');
     let startLoc = 0; // TODO: make this a param
     let readData = 0;
-    console.log(`\n\tErasing I2C memory device at address ${i2cAddress.toString(16)}`);
+    console.log(`\n\treadMemory(): I2C memory device at address ${i2cAddress.toString(16)}`);
     const i2c1 = i2c.openSync(busNum);
 
     for (let byteAddr = startLoc; byteAddr < startLoc + numBytes; byteAddr++) {
@@ -109,7 +132,7 @@ const readMemory = (
 
     i2c1.closeSync();
 };
-
+*/
 
 // time delay
 // we need this because the hardware has a 5ms write time
@@ -131,15 +154,14 @@ const main = () => {
     let totalMemSize = 2048;
     let regionSize = totalMemSize < 256 ? totalMemSize : 256;
     let chipPageSize = 16;
-    let testByte = 0xff; // test data
+    let testByte = 0; // test data
 
     configPins();
 
     // write same byte to all spec mem locs
     try {
         for (let addressLines = A2A1A0start; addressLines <= A2A1A0end; addressLines++) {
-            console.log('erasing 256 bytes at address line= ',
-                addressLines.toString(16));
+            console.log(`erasing ${regionSize} bytes at address line = ${addressLines.toString(16)}` );
             eraseMemory( i2cBusNum, MEMCHIP_ADDR + addressLines,
                 regionSize, testByte, chipPageSize);
         }
